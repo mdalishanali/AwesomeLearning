c16acd75968e4b4b2bb836981c7604978d09f497
MD ALISHAN ALI <md.alishanali88@gmail.com>
2023-01-29 15:21:10 +0530

    project init

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+      <Text>App</Text>
+    </View>
+  )
+}
